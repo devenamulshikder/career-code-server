35fe73459d9047b625fb24db825eab2c0c375898
devenamulshikder <devenamulshikder@gmail.com>
2025-05-27 20:54:50 +0600

    Refactor index.js for improved readability and structure; added ObjectId import and updated MongoDB URI handling.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://<db_username>:<db_password>@cluster0.shu503b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.shu503b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,8 +18,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
 
+    const jobsCollection = client.db("careerCode").collection("jobs");
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/jobs/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query)
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -28,11 +41,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Career Code Server is running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('Career Code Server is running');
-})
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
