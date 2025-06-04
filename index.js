47e9f246da83f4a4891f97ce472ac4b753c6143e
devenamulshikder <devenamulshikder@gmail.com>
2025-06-05 01:01:25 +0600

    update status fieldset using patch method

--- index.js
+++ index.js
@@ -88,6 +88,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: req.body.status,
+        },
+      };
+      const result = await applicationsCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
     app.delete("/applications/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
